24284219154a8bfbb430ff66e9cadaadcca85160
Cloud User <ec2-user@ip-10-0-2-235.ec2.internal>
2016-04-12 23:35:04 -0400

    added statement to remove expired tokens

--- push_notifications.py
+++ push_notifications.py
@@ -5,14 +5,23 @@ import sqlite3
 import sys
 
 database = '/usr/share/nginx/html/api/app/tokens.db'
-gcm = GCM("Api key from google")
+gcm = GCM("AIza.....")
 data = {'message': '{0}'.format(sys.argv[1])}
 
 conn = sqlite3.connect(database)
 c = conn.cursor()
 c.execute('select * from token_table')
 all_rows = c.fetchall()
+expired_list = []
 
 for tokens in all_rows:
     reg_id = tokens[1]
-    gcm.plaintext_request(registration_id=reg_id, data=data)
+    try:
+        gcm.plaintext_request(registration_id=reg_id, data=data)
+    except Exception as e:
+        c.execute('DELETE FROM token_table WHERE data=?', (reg_id,))
+        conn.commit()
+        expired_list.append(reg_id)
+        print e
+
+print expired_list
